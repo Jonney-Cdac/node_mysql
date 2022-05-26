0dff3157a04033d27bbcfa9ba8ab56828fecb1c1
jonathan_49481 <lobojonathan1995@gmail.com>
2022-05-26 15:55:38 +0530

    looping done using map function

---
+++ course/looping_through_db.js
@@ -0,0 +1,33 @@
+var mysql = require("mysql2");
+
+// Create a connection.
+var connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"jonathanlobo1995",
+    database:"node_mysql",
+});
+
+//Connect to the backend.
+
+connection.connect((error)=>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Connection established successfully!!");
+        
+        
+        //Create record in Table
+        const sql = "Select * from softwares";
+        
+        connection.query(sql,(error,result)=>{
+            if(error){
+                console.log(error);
+            }else{
+            result.map((results)=>{
+                console.log(results.name + "|   " +results.cost);
+                });
+            }
+        });
+    }
+});
